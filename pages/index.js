ae459c03dd6137c6de304643730346f9ce6ac327
Jason Laster <jason.laster.11@gmail.com>
2021-03-28 22:06:57 -0700

    add simple change

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+
 export default function Home() {
   return (
     <>
