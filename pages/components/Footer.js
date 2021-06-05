0a7cf81e50c2b32bcff1dc2398d491b1e12e3fa6
Jon Bell <github@lot23.com>
2021-06-05 10:11:53 +0000

    Starting to change the footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,25 +1,161 @@
+const navigation = {
+  company: [
+    { name: "About", href: "#" },
+    { name: "Philosophy", href: "#" },
+    { name: "Privacy Policy", href: "#" }
+  ],
+  product: [
+    { name: "Technology", href: "#" },
+    { name: "Docs", href: "#" },
+    { name: "Use Cases", href: "#" }
+  ],
+  contact: [
+    { name: "hey@replay.io", href: "#" },
+    { name: "Discord", href: "#" },
+    { name: "Twitter", href: "#" },
+    { name: "Github", href: "#" }
+  ],
+  social: [
+    {
+      name: "Twitter",
+      href: "https://twitter.com/replayio",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+        </svg>
+      )
+    },
+    {
+      name: "GitHub",
+      href: "https://github.com/RecordReplay",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      )
+    }
+  ]
+};
+
 export default function Footer() {
-    return (
-        <div>
-            <div className="max-w-4xl mx-auto pb-8 px-4 sm:px-6 sm:pb-12 sm:flex sm:items-center sm:flex-col lg:max-w-7xl lg:px-8 lg:flex lg:flex-row lg:justify-between">
-                <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                    <span className="block">Interested?</span>
-                </h2>
-                <div className="mt-6 space-y-4">
-                    <a
-                        href="https://form.typeform.com/to/jAaabLbi"
-                        data-mode="drawer_right"
-                        data-submit-close-delay="0"
-                        target="_blank"
-                        className="typeform-share flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-500 hover:bg-blue-700"
-                    >
-                        Hop on the beta list!
-                    </a>
-                </div>
+  return (
+    <footer className="bg-white" aria-labelledby="footerHeading">
+      <h2 id="footerHeading" className="sr-only">
+        Footer
+      </h2>
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div className="grid grid-cols-2 gap-8 xl:col-span-2">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+                  Company
+                </h3>
+                <ul className="mt-4 space-y-4">
+                  {navigation.company.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-12 md:mt-0">
+                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+                  Product
+                </h3>
+                <ul className="mt-4 space-y-4">
+                  {navigation.product.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            <div className="flex-row flex align-center border-t-2 border-blue-500 py-2 justify-center text-gray-700">
-                <a href="/privacy.html">Privacy Policy</a>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+                  Contact
+                </h3>
+                <ul className="mt-4 space-y-4">
+                  {navigation.contact.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
+          </div>
+          <div className="mt-8 xl:mt-0">
+            <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+              Subscribe to our newsletter
+            </h3>
+            <p className="mt-4 text-base text-gray-500">
+              The latest news, articles, and resources, sent to your inbox
+              weekly.
+            </p>
+            <form className="mt-4 sm:flex sm:max-w-md">
+              <label htmlFor="emailAddress" className="sr-only">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="emailAddress"
+                id="emailAddress"
+                autoComplete="email"
+                required
+                className="appearance-none min-w-0 w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:placeholder-gray-400"
+                placeholder="Enter your email"
+              />
+              <div className="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+                <button
+                  type="submit"
+                  className="w-full bg-blue-600 flex items-center justify-center border border-transparent rounded-md py-2 px-4 text-base font-medium text-white hover:bg-blue-700 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Subscribe
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+        <div className="mt-8 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between">
+          <div className="flex space-x-6 md:order-2">
+            {navigation.social.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
+          </div>
+          <p className="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
+            &copy; 2021 Replay, All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
